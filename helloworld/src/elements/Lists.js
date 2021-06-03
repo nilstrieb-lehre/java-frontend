90663b28609c1510aeb5feaa24c9de2faea0ddb7
Nilstrieb <nilstrieb@gmail.com>
2021-06-03 11:25:54 +0200

    list

--- helloworld/src/elements/Lists.js
+++ helloworld/src/elements/Lists.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 
-function Lists() {
-    const colors = ["red", "blurple"]
+const Lists = () => {
+    const [colors, setColors] = useState(["red", "blurple"]);
+
+    const newColorInput = useRef(null);
+
     return (
         <div>
             <h2>Colors</h2>
             <ul>
-                {colors.map((color, i) => <li key={i}>{color}</li>)}
+                {colors.map((color, i) =>
+                    <li key={i}>{color}</li>
+                )}
             </ul>
+            <input ref={newColorInput}/>
+            <button onClick={() => setColors(c => [...c, newColorInput.current.value])}>neue farbe</button>
         </div>
     );
-}
+};
 
 export default Lists;
