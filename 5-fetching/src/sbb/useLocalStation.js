aeeb4f446c6c33607915bfc4f73a2ea56c7fe3fa
Nilstrieb <nilstrieb@gmail.com>
2021-06-16 11:20:03 +0200

    corsin is no more

--- 5-fetching/src/sbb/useLocalStation.js
+++ 5-fetching/src/sbb/useLocalStation.js
@@ -1,24 +1,24 @@
-import {useEffect, useState} from "react"; /*corsin*/
-/*corsin*/
-const useLocalStation = () => {/*corsin*/
-    const [location, setLocation] = useState(null);/*corsin*/
-    const [allowed, setAllowed] = useState(true);/*corsin*/
-    /*corsin*/
-    useEffect(() => {/*corsin*/
-        navigator.geolocation.getCurrentPosition(pos => {/*corsin*/
-            fetch(`http://transport.opendata.ch/v1/locations?x=${pos.coords.latitude}&y=${pos.coords.longitude}&type=station`)/*corsin*/
-                .then(response => response.json())/*corsin*/
-                .then(data => {/*corsin*/
-                    if (data.stations) {/*corsin*/
-                        setLocation(data.stations.filter(s => s.id)[0]);/*corsin*/
-                    }/*corsin*/
-                })/*corsin*/
-                .catch(() => {/*corsin*/
-                })/*corsin*/
-        }, () => setAllowed(false));/*corsin*/
-    }, [allowed]);/*corsin*/
-    /*corsin*/
-    return location;/*corsin*/
-}/*corsin*/
-/*corsin*/
-export default useLocalStation;/*corsin*/
+import {useEffect, useState} from "react";
+
+const useLocalStation = () => {
+    const [location, setLocation] = useState(null);
+    const [allowed, setAllowed] = useState(true);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(pos => {
+            fetch(`http://transport.opendata.ch/v1/locations?x=${pos.coords.latitude}&y=${pos.coords.longitude}&type=station`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.stations) {
+                        setLocation(data.stations.filter(s => s.id)[0]);
+                    }
+                })
+                .catch(() => {
+                })
+        }, () => setAllowed(false));
+    }, [allowed]);
+
+    return location;
+}
+
+export default useLocalStation;
